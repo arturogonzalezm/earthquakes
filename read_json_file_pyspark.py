ef4265236dc99052afea49f08aaf905f269e3f92
arturo.gonzalez <arturo.gonzalez@healthdirect.org.au>
2019-04-16 13:25:33 +1000

    0001 - Added approach 1: Added docstrings.

--- read_json_file_pyspark.py
+++ read_json_file_pyspark.py
@@ -1,6 +1,6 @@
 import os
 
-from pyspark.shell import sqlContext, spark
+from pyspark.shell import sqlContext
 
 root = os.path.dirname(__file__)
 path = 'data/all_month.geojson'
@@ -13,9 +13,9 @@ def main():
     data = os.path.join(root, path)
     df = sqlContext.read.json(data)
     df.createOrReplaceTempView('earthquakes')
-    earthquakes_df = spark.sql("SELECT properties.mag, properties.place "
-                               "FROM earthquakes "
-                               "WHERE properties.mag > 1.0")
+    earthquakes_df = sqlContext.sql("SELECT properties.mag, properties.place "
+                                    "FROM earthquakes "
+                                    "WHERE properties.mag > 1.0")
     earthquakes_df.show()
 
 
