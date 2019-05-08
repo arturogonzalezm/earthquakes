1afda35a373b3d31d6e912db4d17d3b66e70bc9c
Arturo Gonzalez <arturo@arturosolutions.com.au>
2019-05-08 15:06:43 +1000

    0005 - Updated: Code formatting and implemented function.

--- read_json_url_requests.py
+++ read_json_url_requests.py
@@ -1,17 +1,26 @@
 import requests
 import time
 
+url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
-def main():
+
+def load_json_data(json_url):
     """
+    :param json_url: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+    :return: Earthquakes json data
+    """
+    data = requests.get(json_url).json()
+    return data
+
+
+def get_place_and_magnitude(data):
+    """
+    :param data: Earthquakes json data
     :return: List of places and magnitudes, where magnitude is greater than 1.0.
     """
     start = time.time()
 
-    url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
-    response = requests.get(url)
-    data = response.json()  # Check the JSON Response Content documentation below
-
+    data = load_json_data(url)
     for dictionary in data['features']:
         place = dictionary['properties']['place']
         magnitude = dictionary['properties']['mag']
@@ -23,7 +32,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    get_place_and_magnitude(url)
 
 # Python3.6: 4.863826036453247
 # PyPy3: 4.521245002746582
